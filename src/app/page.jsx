7b6908ea48bb0055a5976bc01acce4502c0e3b44
jermz876 <109403054+jermz876@users.noreply.github.com>
2023-12-07 23:36:08 -0500

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main>
+            <section>
+                <h1>Hello World</h1>
+            </section>
+        </main>
+    );
+}
